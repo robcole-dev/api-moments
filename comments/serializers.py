8de9a41337f29545654a903ca45e373b71da2118
Rob Cole <rob-cole@hotmail.co.uk>
2022-08-09 16:28:51 +0000

    Add generic views

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,6 +3,10 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -13,11 +17,11 @@ class CommentSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Comment()
+        model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'post', 'content']
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
+        ]
 
 
 class CommentDetailSerializer(CommentSerializer):
