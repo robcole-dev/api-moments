885cf2544a0ab08079e3577a33e2586c91af6a91
Rob Cole <rob-cole@hotmail.co.uk>
2022-08-09 20:39:35 +0000

    Final Commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -46,7 +46,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'moments-api-rc.herokuapp.com',
